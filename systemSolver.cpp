3e47b7ef385e850edc84c4d34135dbe9f875f9ac
sa406414 <142535624+sa406414@users.noreply.github.com>
2024-08-21 13:09:15 -0500

    Update systemSolver.cpp after compiler

--- systemSolver.cpp
+++ systemSolver.cpp
@@ -1,44 +1,6 @@
-#include util.h
+#include "util.h"
 
 using namespace std;
-
-
-int main()
-{
-    string input = "x";
-    vector<vector<vector<int>>> savedMatrices {};
-    vector<vector<int>> matrice {};
-    vector<int> coeff {};
-    int equations;
-    int unknowns;
-    int systnum;
-    while (input != "e")
-    {
-        if (input == "c")
-        {
-            equations = readInt(2, 9, "Enter number of equations: ", "Must be greater than 2: ");
-            unknowns = readInt(2, 9, "Enter number of unknowns: ", "Must be greater than 2: ");
-            savedMatrices.pushback(createMatrice(equations, unknowns));
-        }
-        if (input == "d")
-        {
-            cout << "You have " << savedMatrices.size() << "saved systems";
-            systnum = readInt(1, savedMatrices.size(), "Display system: ");
-            displayEquations(savedMatrices[systnum - 1]);
-        }
-        if (input == "s")
-        {
-
-        }
-        if (input == "del")
-        {
-
-        }
-        cout << "(c)reate new system" << endl << "(d)isplay existing system" << endl << "(s)olve existing system" << endl << "(del)ete a system" << endl << "(e)xit";
-        getline(cin, input);
-    }
-    return 0;
-}
 
 vector<vector<int>> createMatrice(int equations, int unknowns)
 {
@@ -55,15 +17,15 @@ vector<vector<int>> createMatrice(int equations, int unknowns)
             if (v != unknowns)
             {
                 coefficient = readInt("Enter coefficient of " + var + ": ");
-                coeff.pushback(coefficient);
+                coeff.push_back(coefficient);
             }
             else
             {
                 coefficient = readInt("Enter answer of the equation: ");
-                coeff.pushback(coefficient);
+                coeff.push_back(coefficient);
             }
         }
-        matrice.pushback(coeff);
+        matrice.push_back(coeff);
     }
     return matrice;
 }
@@ -85,15 +47,55 @@ void displayEquations(vector<vector<int>> matrice)
 
 vector<int> solve(vector<vector<int>> matrice)
 {
-
+    //placeholder
+    return matrice[0];
 }
 
 vector<vector<int>> addRow(vector<vector<int>> matrice, vector<int> row1, vector<int> row2)
 {
-
+    //placeholer
+    return matrice;
 }
 
-vector<vector<int>> multiplyRow(vvector<vector<int>> matrice, vector<int> row1, vector<int> row2)
+vector<vector<int>> multiplyRow(vector<vector<int>> matrice, vector<int> row1, vector<int> row2)
 {
+    //placeholder
+    return matrice;
+}
 
+int main()
+{
+    string input = "x";
+    vector<vector<vector<int>>> savedMatrices {};
+    vector<vector<int>> matrice {};
+    vector<int> coeff {};
+    int equations;
+    int unknowns;
+    int systnum;
+    while (input != "e")
+    {
+        if (input == "c")
+        {
+            equations = readInt(2, 9, "Enter number of equations: ", "Must be greater than 2: ");
+            unknowns = readInt(2, 9, "Enter number of unknowns: ", "Must be greater than 2: ");
+            savedMatrices.push_back(createMatrice(equations, unknowns));
+        }
+        if (input == "d")
+        {
+            cout << "You have " << savedMatrices.size() << "saved systems";
+            systnum = readInt(1, savedMatrices.size(), "Display system: ");
+            displayEquations(savedMatrices[systnum - 1]);
+        }
+        if (input == "s")
+        {
+
+        }
+        if (input == "del")
+        {
+
+        }
+        cout << "(c)reate new system" << endl << "(d)isplay existing system" << endl << "(s)olve existing system" << endl << "(del)ete a system" << endl << "(e)xit";
+        getline(cin, input);
+    }
+    return 0;
 }
